98aae3a5bb8540bc127e8a9f00b453798fe63b73
Robert Kroeger <rjkroege@chromium.org>
2018-02-06 06:14:04 -0500

    [frame] Add package comment.

---
+++ frame/doc.go
@@ -0,0 +1,6 @@
+package frame
+
+// Package frame supports frames of editable text in a single font on
+// raster displays, such as in sam(1) and 9term(1). Frames may hold any
+// character except NUL (0). Long lines are folded and tabs are at fixed
+// intervals.
