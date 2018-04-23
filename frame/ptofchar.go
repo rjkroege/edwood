60708779eef8fff6694b48ff58b8383b45bcbdaf
Robert Kroeger <rjkroege@chromium.org>
2018-04-23 04:19:17 -0700

    [frame] fix rune/byte bug in Ptofchar
    Ptofchar was only successfully measuring the width of single-byte
    characters in byte slices. Fix this.

--- frame/ptofchar.go
+++ frame/ptofchar.go
@@ -17,7 +17,7 @@ func (f *Frame) ptofcharptb(p int, pt image.Point, bn int) image.Point {
 				for s := 0; s < len(b.Ptr) && p > 0; s += w {
 					p--
 					r, w = utf8.DecodeRune(b.Ptr[s:])
-					pt.X += f.Font.StringWidth(string(b.Ptr[s : s+1]))
+					pt.X += f.Font.BytesWidth(b.Ptr[s : s+w])
 					if r == utf8.RuneError || pt.X > f.Rect.Max.X {
 						panic("frptofchar")
 					}
