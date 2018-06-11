37e3255000c35ff7c3d70b7a35da00a9533f78c9
Robert Kroeger <rjkroege@chromium.org>
2018-06-11 08:17:00 -0400

    gofmt row_test.go
    Make row_test.go appropriately formatted.

--- row_test.go
+++ row_test.go
@@ -1,35 +1,28 @@
 package main
 
 import (
-	"os"
 	"io/ioutil"
+	"os"
 	"path/filepath"
-	"testing"
 	"reflect"
+	"testing"
 )
 
-	type loadfontstest struct {
-		fname string
-		val string
-		
-		
-	}
+type loadfontstest struct {
+	fname string
+	val   string
+}
 
-const short_file = 
-`/Users/rjkroege/tools/gopkg/src/github.com/rjkroege/edwood
+const short_file = `/Users/rjkroege/tools/gopkg/src/github.com/rjkroege/edwood
 /mnt/font/GoRegular/13a/font
 `
 
-const full_file =
-`/Users/rjkroege/tools/gopkg/src/github.com/rjkroege/edwood
+const full_file = `/Users/rjkroege/tools/gopkg/src/github.com/rjkroege/edwood
 /mnt/font/GoRegular/13a/font
 /mnt/font/Iosevka/12a/font
 `
 
 func TestLoadFonts(t *testing.T) {
-	
-	// write a directory
-	
 	dir, err := ioutil.TempDir("", "testloadfonts")
 	if err != nil {
 		t.Fatal("TestLoadFonts can't make directory:", err)
@@ -58,11 +51,11 @@ func TestLoadFonts(t *testing.T) {
 		t.Fatal("TestLoadFonts short_file can't write file:", err)
 	}
 	f.Close()
-	
+
 	if resp := LoadFonts(filepath.Join(dir, "short_file")); !reflect.DeepEqual(resp, []string{}) {
 		t.Errorf("TestLoadFonts short_file want %v, got %v", []string{}, resp)
 	}
-	
+
 	f, err = os.Create(filepath.Join(dir, "full_file"))
 	if err != nil {
 		t.Fatal("TestLoadFonts full_file can't create file:", err)
@@ -71,14 +64,14 @@ func TestLoadFonts(t *testing.T) {
 		t.Fatal("TestLoadFonts full_file can't write file:", err)
 	}
 	f.Close()
-	
+
 	if resp := LoadFonts(filepath.Join(dir, "full_file")); !reflect.DeepEqual(resp, []string{
-			"/mnt/font/GoRegular/13a/font",
-			"/mnt/font/Iosevka/12a/font",
- 	}) {
+		"/mnt/font/GoRegular/13a/font",
+		"/mnt/font/Iosevka/12a/font",
+	}) {
 		t.Errorf("TestLoadFonts full_file want %v, got %v", []string{
 			"/mnt/font/GoRegular/13a/font",
 			"/mnt/font/Iosevka/12a/font",
- 	}, resp)
+		}, resp)
 	}
 }
