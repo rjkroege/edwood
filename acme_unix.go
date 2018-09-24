4d4130e7c6ad5b9437ffd2bb30377aa7989403dd
Fazlul Shahriar <fshahriar@gmail.com>
2018-09-24 14:27:00 -0400

    Change default font in Plan 9 (#123)
    "lucidasans" Plan 9 font is called "lucsans" in plan9port
    as part of an agreement with Bigelow & Holmes.
    See https://marc.info/?l=9fans&m=114412454010468&w=2
    
    Helps #115

---
+++ acme_unix.go
@@ -0,0 +1,25 @@
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+
+package main
+
+import (
+	"os"
+	"syscall"
+)
+
+// lucidasans font is called lucsans in plan9port.
+// See https://marc.info/?l=9fans&m=114412454010468&w=2
+const defaultVarFont = "/lib/font/bit/lucsans/euro.8.font"
+
+var ignoreSignals = []os.Signal{
+	syscall.SIGPIPE,
+	syscall.SIGTTIN,
+	syscall.SIGTTOU,
+	syscall.SIGTSTP,
+}
+
+var hangupSignals = []os.Signal{
+	syscall.SIGINT,
+	syscall.SIGTERM,
+	syscall.SIGHUP,
+}
