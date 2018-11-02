4248bbe6151be33578d81bceb693ec360355c6c0
Robert Kroeger <rjkroege@chromium.org>
2018-11-02 08:32:04 -0400

    more doc changes

--- frame/doc.go
+++ frame/doc.go
@@ -1,6 +1,8 @@
-package frame
-
-// Package frame supports frames of editable text in a single font on
-// raster displays, such as in sam(1) and 9term(1). Frames may hold any
+// Package frame supports frames of editable text.
+// 
+// This is a port of Plan9's libframe to Go. It supports displaying
+// a frame of editable text in a single font on
+// raster displays, such as would be found in sam(1) and 9term(1). Frames may hold any
 // character except NUL (0). Long lines are folded and tabs are at fixed
 // intervals.
+package frame
