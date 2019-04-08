51117b6b09ae90582d578dfb80f7269c26fccd3c
Fazlul Shahriar <fshahriar@gmail.com>
2019-04-08 19:30:37 -0400

    Make plan9port optional with 'duitdraw mux9p' tags
    * Also create plan9port namespace directory if it doesn't exist.

--- acme_p9p.go
+++ acme_p9p.go
@@ -1,5 +1,5 @@
 // +build darwin dragonfly freebsd linux netbsd openbsd solaris
-// +build duitdraw
+// +build !duitdraw
 
 package main
 
@@ -9,9 +9,12 @@ import (
 )
 
 const (
-	defaultVarFont   = ""
-	defaultFixedFont = ""
-	defaultMtpt      = ""
+	// lucidasans font is called lucsans in plan9port.
+	// See https://marc.info/?l=9fans&m=114412454010468&w=2
+	defaultVarFont   = "/lib/font/bit/lucsans/euro.8.font"
+	defaultFixedFont = "/lib/font/bit/lucm/unicode.9.font"
+
+	defaultMtpt = ""
 )
 
 var ignoreSignals = []os.Signal{
