rjkroege/edwood

commit 4f4fb76e0bdce1ab367396f5cd3d4abfcbb846c8

Document and test Mnt/MntDir

fsys.go
@@ -82,10 +82,13 @@ var dirtabw = []*DirTab{
 	//	{ nil, }
 }
 
+// Mnt is a collection of reference counted MntDir.
+// It is used to pass information from Edwood's 9p client to
+// Edwood's 9p file server.
 type Mnt struct {
 	lk sync.Mutex
-	id int
-	md *MntDir
+	id int     // Used to generate MntDir identifier.
+	md *MntDir // Linked list of MntDir.
 }
 
 var mnt Mnt
@@ -151,32 +154,31 @@ func (fs *fileServer) fsysproc() {
 	}
 }
 
-func fsysaddid(dir string, incl []string) *MntDir {
+// Add creates a new MntDir and returns a new reference to it.
+func (mnt *Mnt) Add(dir string, incl []string) *MntDir {
 	mnt.lk.Lock()
 	mnt.id++
-	id := mnt.id
-	m := &MntDir{}
-	m.id = int64(id)
-	m.dir = dir
-	m.ref = 1 // one for Command, one will be incremented in attach
-	m.next = mnt.md
-	m.incl = incl
+	m := &MntDir{
+		id:   int64(mnt.id),
+		ref:  1, // One for Command. Incremented in attach, walk, etc.
+		next: mnt.md,
+		dir:  dir,
+		incl: incl,
+	}
 	mnt.md = m
 	mnt.lk.Unlock()
 	return m
 }
 
-func fsysincid(m *MntDir) {
+// IncRef increments reference to given MntDir.
+func (mnt *Mnt) IncRef(m *MntDir) {
 	mnt.lk.Lock()
 	m.ref++
 	mnt.lk.Unlock()
 }
 
-func fsysdelid(idm *MntDir) {
-	var (
-		m, prev *MntDir
-	)
-
+// DecRef decrements reference to given MntDir.
+func (mnt *Mnt) DecRef(idm *MntDir) {
 	if idm == nil {
 		return
 	}
@@ -186,8 +188,8 @@ func fsysdelid(idm *MntDir) {
 	if idm.ref > 0 {
 		return
 	}
-	prev = nil
-	for m = mnt.md; m != nil; m = m.next {
+	var prev *MntDir
+	for m := mnt.md; m != nil; m = m.next {
 		if m == idm {
 			if prev != nil {
 				prev.next = m.next
@@ -199,7 +201,21 @@ func fsysdelid(idm *MntDir) {
 		prev = m
 	}
 
-	cerr <- fmt.Errorf("fsysdelid: can't find id %d", idm.id)
+	cerr <- fmt.Errorf("Mnt.DecRef: can't find id %d", idm.id)
+}
+
+// GetFromID finds the MntDir with given id and returns a new reference to it.
+func (mnt *Mnt) GetFromID(id int64) *MntDir {
+	mnt.lk.Lock()
+	defer mnt.lk.Unlock()
+
+	for m := mnt.md; m != nil; m = m.next {
+		if m.id == id {
+			m.ref++
+			return m
+		}
+	}
+	return nil
 }
 
 func (fs *fileServer) close() {
@@ -276,19 +292,10 @@ func (fs *fileServer) attach(x *Xfid, f *Fid) *Xfid {
 			acmeerror(fmt.Sprintf("fsysattach: bad Aname %s", x.fcall.Aname), err)
 		}
 	}
-	mnt.lk.Lock()
-	var m *MntDir
-	for m = mnt.md; m != nil; m = m.next {
-		if m.id == id {
-			f.mntdir = m
-			m.ref++
-			break
-		}
-	}
+	m := mnt.GetFromID(id) // DecRef in clunk
 	if m == nil && x.fcall.Aname != "" {
 		cerr <- fmt.Errorf("unknown id '%s' in attach", x.fcall.Aname)
 	}
-	mnt.lk.Unlock()
 	return fs.respond(x, &t, nil)
 }
 
@@ -316,7 +323,7 @@ func (fs *fileServer) walk(x *Xfid, f *Fid) *Xfid {
 		nf.open = false
 		nf.mntdir = f.mntdir
 		if f.mntdir != nil {
-			f.mntdir.ref++
+			mnt.IncRef(f.mntdir) // DecRef in clunk
 		}
 		nf.dir = f.dir
 		nf.qid = f.qid
@@ -447,7 +454,7 @@ func (fs *fileServer) walk(x *Xfid, f *Fid) *Xfid {
 	if err != nil || len(t.Wqid) < len(x.fcall.Wname) {
 		if nf != nil {
 			nf.busy = false
-			fsysdelid(nf.mntdir)
+			mnt.DecRef(nf.mntdir)
 		}
 	} else {
 		if len(t.Wqid) == len(x.fcall.Wname) {
@@ -592,7 +599,7 @@ func (fs *fileServer) write(x *Xfid, f *Fid) *Xfid {
 }
 
 func (fs *fileServer) clunk(x *Xfid, f *Fid) *Xfid {
-	fsysdelid(f.mntdir)
+	mnt.DecRef(f.mntdir) // IncRef in attach/walk
 	x.c <- xfidclose
 	return nil
 }

acme.go
@@ -509,7 +509,7 @@ func waitthread() {
 			if c.iseditcommand {
 				cedit <- 0
 			}
-			fsysdelid(c.md)
+			mnt.DecRef(c.md) // mnt.Add in fsysmount
 		}
 	}
 	for {

dat.go
@@ -150,11 +150,18 @@ type DirTab struct {
 	perm uint
 }
 
+// MntDir contains context of where an external command was run.
 type MntDir struct {
-	id   int64
-	ref  int
-	dir  string
-	next *MntDir
+	id   int64   // Unique identifier used as Aname in Tattach.
+	ref  int     // Used for reference counting.
+	next *MntDir // Next in linked list.
+
+	// Directory where the command was run.
+	// Writes to cons file go to window named dir+"/+Errors".
+	dir string
+
+	// Additional search paths for C #include inherited from the window
+	// where the command was run.
 	incl []string
 }
 

fsys_p9p.go
@@ -121,18 +121,18 @@ func post9pservice(conn net.Conn, name string, mtpt string) error {
 
 // Called only in exec.c:/^run(), from a different FD group
 func fsysmount(dir string, incl []string) (*MntDir, *client.Fsys, error) {
-	md := fsysaddid(dir, incl)
+	md := mnt.Add(dir, incl) // DecRef in waitthread
 	if md == nil {
 		return nil, nil, fmt.Errorf("child: can't allocate mntdir")
 	}
 	conn, err := client.DialService("acme")
 	if err != nil {
-		fsysdelid(md)
+		mnt.DecRef(md)
 		return nil, nil, fmt.Errorf("child: can't connect to acme: %v", err)
 	}
 	fs, err := conn.Attach(nil, getuser(), fmt.Sprintf("%d", md.id))
 	if err != nil {
-		fsysdelid(md)
+		mnt.DecRef(md)
 		return nil, nil, fmt.Errorf("child: can't attach to acme: %v", err)
 	}
 	return md, fs, nil

fsys_plan9.go
@@ -66,7 +66,7 @@ func post9pservice(conn net.Conn, name string, mtpt string) error {
 
 // Called only in exec.c:/^run(), from a different FD group
 func fsysmount(dir string, incl []string) (*MntDir, *client.Fsys, error) {
-	md := fsysaddid(dir, incl)
+	md := mnt.Add(dir, incl) // DecRef in waitthread
 	if md == nil {
 		return nil, nil, fmt.Errorf("child: can't allocate mntdir")
 	}

fsys_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"reflect"
 	"runtime"
 	"strings"
 	"testing"
@@ -508,5 +509,84 @@ func (tfs tFsys) Write(file, s string) {
 func TestGetuser(t *testing.T) {
 	if getuser() == "" {
 		t.Errorf("Didn't get a username")
+	}
+}
+
+func TestMnt(t *testing.T) {
+	var mnt Mnt
+
+	md := mnt.GetFromID(1)
+	if md != nil {
+		t.Errorf("mnt.Find(1) returned %v; expected nil", md)
+	}
+
+	md = mnt.Add("/home/gopher", nil)
+	want := &MntDir{
+		id:   1,
+		ref:  1,
+		next: nil,
+		dir:  "/home/gopher",
+		incl: nil,
+	}
+	if !reflect.DeepEqual(md, want) {
+		t.Fatalf("mnt.Add returned %v; expected %v", md, want)
+	}
+
+	mnt.IncRef(md)
+	if got, want := md.ref, 2; got != want {
+		t.Fatalf("mnt.IncRef increased ref to %v; expected %v", got, want)
+	}
+
+	mnt.DecRef(md)
+	if got, want := md.ref, 1; got != want {
+		t.Fatalf("mnt.DecRef decreased ref to %v; expected %v", got, want)
+	}
+
+	if got, want := mnt.GetFromID(1), md; got != want {
+		t.Fatalf("mnt.Find returned %v; expected %v", got, want)
+	}
+	if got, want := md.ref, 2; got != want {
+		t.Fatalf("Find increased ref to %v; expected %v", got, want)
+	}
+
+	mnt.DecRef(md)
+	if got, want := md.ref, 1; got != want {
+		t.Fatalf("mnt.DecRef decreased ref to %v; expected %v", got, want)
+	}
+
+	mnt.DecRef(md)
+	if got, want := md.ref, 0; got != want {
+		t.Fatalf("mnt.DecRef decreased ref to %v; expected %v", got, want)
+	}
+	if mnt.md != nil {
+		t.Fatalf("mnt.md is %v; expected nil", mnt.md)
+	}
+}
+
+func TestMntDecRef(t *testing.T) {
+	var mnt Mnt
+	mnt.DecRef(nil) // no-op
+
+	md := &MntDir{id: 42}
+	cerr = make(chan error)
+	go mnt.DecRef(md)
+	err := <-cerr
+	wantErr := fmt.Sprintf("Mnt.DecRef: can't find id %d", md.id)
+	if err == nil || err.Error() != wantErr {
+		t.Errorf("mnt.DecRef invalid id %d generated error %v; expected %q", md.id, err, wantErr)
+	}
+
+	md1 := mnt.Add("/home/gopher1", nil)
+	md2 := mnt.Add("/home/gopher2", nil)
+	if mnt.md != md2 || md2.next != md1 || md1.next != nil {
+		t.Fatalf("linked list (got/want): %p/%p > %p/%p > %p/nil", mnt.md, md2, md2.next, md1, md1.next)
+	}
+	mnt.DecRef(md1)
+	if mnt.md != md2 || md2.next != nil {
+		t.Fatalf("linked list (got/want): %p/%p > %p/nil", mnt.md, md2, md2.next)
+	}
+	mnt.DecRef(md2)
+	if mnt.md != nil {
+		t.Fatalf("mnt.md is %v; expected nil", mnt.md)
 	}
 }

fsys_unix.go
@@ -39,18 +39,18 @@ func post9pservice(conn net.Conn, name string, mtpt string) error {
 
 // Called only in exec.c:/^run(), from a different FD group
 func fsysmount(dir string, incl []string) (*MntDir, *client.Fsys, error) {
-	md := fsysaddid(dir, incl)
+	md := mnt.Add(dir, incl) // DecRef in waitthread
 	if md == nil {
 		return nil, nil, fmt.Errorf("child: can't allocate mntdir")
 	}
 	conn, err := client.DialService("acme")
 	if err != nil {
-		fsysdelid(md)
+		mnt.DecRef(md)
 		return nil, nil, fmt.Errorf("child: can't connect to acme: %v", err)
 	}
 	fs, err := conn.Attach(nil, getuser(), fmt.Sprintf("%d", md.id))
 	if err != nil {
-		fsysdelid(md)
+		mnt.DecRef(md)
 		return nil, nil, fmt.Errorf("child: can't attach to acme: %v", err)
 	}
 	return md, fs, nil

fsys_windows.go
@@ -37,18 +37,18 @@ func post9pservice(conn net.Conn, name string, mtpt string) error {
 
 // Called only in exec.c:/^run(), from a different FD group
 func fsysmount(dir string, incl []string) (*MntDir, *client.Fsys, error) {
-	md := fsysaddid(dir, incl)
+	md := mnt.Add(dir, incl) // DecRef in waitthread
 	if md == nil {
 		return nil, nil, fmt.Errorf("child: can't allocate mntdir")
 	}
 	conn, err := client.Dial(fsysAddr.Network(), fsysAddr.String())
 	if err != nil {
-		fsysdelid(md)
+		mnt.DecRef(md)
 		return nil, nil, fmt.Errorf("child: can't connect to acme: %v", err)
 	}
 	fs, err := conn.Attach(nil, getuser(), fmt.Sprintf("%d", md.id))
 	if err != nil {
-		fsysdelid(md)
+		mnt.DecRef(md)
 		return nil, nil, fmt.Errorf("child: can't attach to acme: %v", err)
 	}
 	return md, fs, nil

commit a42e4334bc69a644440785c38bb813a5da46e102

Fix Fid.mntdir not being set on Tattach
I broke this in 4f4fb76e0bdce1ab367396f5cd3d4abfcbb846c8.

fsys.go
@@ -288,6 +288,9 @@ func (fs *fileServer) attach(x *Xfid, f *Fid) *Xfid {
 	if m == nil && x.fcall.Aname != "" {
 		cerr <- fmt.Errorf("unknown id '%s' in attach", x.fcall.Aname)
 	}
+	if m != nil {
+		f.mntdir = m
+	}
 	return fs.respond(x, &t, nil)
 }
 