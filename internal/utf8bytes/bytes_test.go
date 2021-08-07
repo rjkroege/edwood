c6c0b8c21a2727e6f50a93cfbaa7ff6f62c45ef8
camsn0w <camhconnell@gmail.com>
2021-08-07 17:56:32 -0400

    Changed testing variable from bytes to b so I can use the bytes library in testing

--- internal/utf8bytes/bytes_test.go
+++ internal/utf8bytes/bytes_test.go
@@ -5,7 +5,6 @@
 package utf8bytes
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 	"unicode/utf8"
@@ -23,13 +22,13 @@ var testStrings = []string{
 func TestScanForwards(t *testing.T) {
 	for _, s := range testStrings {
 		runes := []rune(s)
-		bytes := NewBytes([]byte(s))
-		if bytes.RuneCount() != len(runes) {
-			t.Errorf("%s: expected %d runes; got %d", s, len(runes), bytes.RuneCount())
+		b := NewBytes([]byte(s))
+		if b.RuneCount() != len(runes) {
+			t.Errorf("%s: expected %d runes; got %d", s, len(runes), b.RuneCount())
 			break
 		}
 		for i, expect := range runes {
-			got := bytes.At(i)
+			got := b.At(i)
 			if got != expect {
 				t.Errorf("%s[%d]: expected %c (%U); got %c (%U)", s, i, expect, expect, got, got)
 			}
@@ -40,14 +39,14 @@ func TestScanForwards(t *testing.T) {
 func TestScanBackwards(t *testing.T) {
 	for _, s := range testStrings {
 		runes := []rune(s)
-		bytes := NewBytes([]byte(s))
-		if bytes.RuneCount() != len(runes) {
-			t.Errorf("%s: expected %d runes; got %d", s, len(runes), bytes.RuneCount())
+		b := NewBytes([]byte(s))
+		if b.RuneCount() != len(runes) {
+			t.Errorf("%s: expected %d runes; got %d", s, len(runes), b.RuneCount())
 			break
 		}
 		for i := len(runes) - 1; i >= 0; i-- {
 			expect := runes[i]
-			got := bytes.At(i)
+			got := b.At(i)
 			if got != expect {
 				t.Errorf("%s[%d]: expected %c (%U); got %c (%U)", s, i, expect, expect, got, got)
 			}
@@ -68,15 +67,15 @@ func TestRandomAccess(t *testing.T) {
 			continue
 		}
 		runes := []rune(s)
-		bytes := NewBytes([]byte(s))
-		if bytes.RuneCount() != len(runes) {
-			t.Errorf("%s: expected %d runes; got %d", s, len(runes), bytes.RuneCount())
+		b := NewBytes([]byte(s))
+		if b.RuneCount() != len(runes) {
+			t.Errorf("%s: expected %d runes; got %d", s, len(runes), b.RuneCount())
 			break
 		}
 		for j := 0; j < randCount(); j++ {
 			i := rand.Intn(len(runes))
 			expect := runes[i]
-			got := bytes.At(i)
+			got := b.At(i)
 			if got != expect {
 				t.Errorf("%s[%d]: expected %c (%U); got %c (%U)", s, i, expect, expect, got, got)
 			}
@@ -90,9 +89,9 @@ func TestRandomSliceAccess(t *testing.T) {
 			continue
 		}
 		runes := []rune(s)
-		bytes := NewBytes([]byte(s))
-		if bytes.RuneCount() != len(runes) {
-			t.Errorf("%s: expected %d runes; got %d", s, len(runes), bytes.RuneCount())
+		b := NewBytes([]byte(s))
+		if b.RuneCount() != len(runes) {
+			t.Errorf("%s: expected %d runes; got %d", s, len(runes), b.RuneCount())
 			break
 		}
 		for k := 0; k < randCount(); k++ {
@@ -102,7 +101,7 @@ func TestRandomSliceAccess(t *testing.T) {
 				continue
 			}
 			expect := string(runes[i:j])
-			got := string(bytes.Slice(i, j))
+			got := string(b.Slice(i, j))
 			if got != expect {
 				t.Errorf("%s[%d:%d]: expected %q got %q", s, i, j, expect, got)
 			}
@@ -112,15 +111,15 @@ func TestRandomSliceAccess(t *testing.T) {
 
 func TestLimitSliceAccess(t *testing.T) {
 	for _, s := range testStrings {
-		bytes := NewBytes([]byte(s))
+		b := NewBytes([]byte(s))
 
-		if string(bytes.Slice(0, 0)) != "" {
+		if string(b.Slice(0, 0)) != "" {
 			t.Error("failure with empty slice at beginning")
 			t.Error("Failed with string: ", s)
 		}
 		nr := utf8.RuneCountInString(s)
 
-		if string(bytes.Slice(nr, nr)) != "" {
+		if string(b.Slice(nr, nr)) != "" {
 			t.Error("failure with empty slice at end")
 		}
 	}
