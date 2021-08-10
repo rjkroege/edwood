d99c4b4f58040269f08e9a095122e936c9f6e6ad
camsn0w <camhconnell@gmail.com>
2021-08-10 13:28:08 -0400

    Separated imports

--- ecmd.go
+++ ecmd.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/rjkroege/edwood/internal/util"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/rjkroege/edwood/internal/util"
 )
 
 var (
