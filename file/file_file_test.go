454bcef70a47e6c8633f7f14f634172a9338bdfe
Robert Kroeger <rjkroege@liqui.org>
2021-10-14 15:51:45 -0400

    Replace fragile TestWindowUndoSelection test

---
+++ file/file_file_test.go
@@ -0,0 +1,65 @@
+// Tests specific to the file.File implementation.
+package file
+
+import (
+	"testing"
+
+	"github.com/rjkroege/edwood/sam"
+)
+
+// Tests that File.Undo correctly reports selection values across Undo
+// actions and does not fail if the File's delta or epsilon arrays are
+// nil. This test is derived from TestWindowUndoSelection. That test was
+// fragile as it reached into specific details of the file.File
+// implementation where file.File should have been an opaque detail of
+// the implementation of the file package.
+func TestFileHandlesNilEpsilonDelta(t *testing.T) {
+	var (
+		word = RuneArray("hello")
+		p0   = 3
+		undo = &Undo{
+			T:   sam.Insert,
+			Buf: word,
+			P0:  p0,
+			N:   word.Nc(),
+		}
+	)
+	for _, tc := range []struct {
+		name           string
+		isundo         bool
+		q0, q1         int
+		wantQ0, wantQ1 int
+		delta, epsilon []*Undo
+	}{
+		{"undo", true, 14, 17, p0, p0 + word.Nc(), []*Undo{undo}, nil},
+		{"redo", false, 14, 17, p0, p0 + word.Nc(), nil, []*Undo{undo}},
+		{"undo (nil delta)", true, 14, 17, 14, 17, nil, nil},
+		{"redo (nil epsilon)", false, 14, 17, 14, 17, nil, nil},
+	} {
+/*
+		w := &Window{
+			body: Text{
+				q0:   tc.q0,
+				q1:   tc.q1,
+				file: file.MakeObservableEditableBuffer("", []rune("This is an example sentence.\n")),
+			},
+		}
+*/
+
+		oeb := MakeObservableEditableBuffer("", []rune("This is an example sentence.\n"))
+		oeb.f.delta = tc.delta
+		oeb.f.epsilon = tc.epsilon
+		
+//		w.body.file.SetDelta(tc.delta)
+//		w.body.file.SetEpsilon(tc.epsilon)
+		q0, q1 := tc.q0, tc.q1
+		if nq0, nq1, hazselection := oeb.Undo(tc.isundo); hazselection {
+			q0, q1 = nq0, nq1
+		}
+
+		if q0 != tc.wantQ0 || q1 != tc.wantQ1 {
+			t.Errorf("%v changed q0, q1 to %v, %v; want %v, %v",
+				tc.name, q0, q1, tc.wantQ0, tc.wantQ1)
+		}
+	}
+}
