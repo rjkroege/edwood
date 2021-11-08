c429594e01ebe6f6c569e1348aa4cd1c331b7e31
camsn0w <camhconnell@gmail.com>
2021-11-08 11:01:31 -0700

    Added the add and String functions to offset_tuple.go

--- file/offset_tuple.go
+++ file/offset_tuple.go
@@ -1,6 +1,20 @@
 package file
 
+import "fmt"
+
 type OffSetTuple struct {
 	b int64
 	r int64
+}
+
+// can work this api to be a little better...
+func (o OffSetTuple) add(b, r int64) OffSetTuple {
+	return OffSetTuple{
+		b: o.b + b,
+		r: o.r + r,
+	}
+}
+
+func (o OffSetTuple) String() string {
+	return fmt.Sprintf("offsettuple b: %d r: %d", o.b, o.r)
 }
