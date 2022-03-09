0af2f8488da0d6764a457ae4d3ee338703169586
Robert Kroeger <rjkroege@liqui.org>
2022-03-09 06:28:37 -0500

    Add a file.BufferCursor
    Add file.BufferCursor: an implementation of io.RuneReader using a
    backing file.Buffer.

--- file/offset_tuple.go
+++ file/offset_tuple.go
@@ -1,6 +1,9 @@
 package file
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 type OffsetTuple struct {
 	B int
@@ -23,4 +26,38 @@ func (o OffsetTuple) decrement(p *piece) OffsetTuple {
 		o.B-len(p.data),
 		o.R-p.nr,
 	)
+}
+
+func (p0 OffsetTuple) Less(p1 OffsetTuple) bool {
+	return p0.R < p1.R
+}
+
+var (
+	// Force that BufferCursor is an io.RuneReader.
+	_ io.RuneReader = (*BufferCursor)(nil)
+
+	// Implement other interfaces as convenient.
+)
+
+type BufferCursor struct {
+	p0 OffsetTuple
+	p1 OffsetTuple
+	b  *Buffer
+}
+
+func MakeBufferCursor(b *Buffer, p0, p1 OffsetTuple) *BufferCursor {
+	return &BufferCursor{
+		b:  b,
+		p0: p0,
+		p1: p1,
+	}
+}
+
+func (cursor *BufferCursor) ReadRune() (rune, int, error) {
+	if cursor.p0.Less(cursor.p1) {
+		r, size, err := cursor.b.ReadRuneAt(cursor.p0)
+		cursor.p0 = cursor.b.RuneTuple(cursor.p0.R + 1)
+		return r, size, err
+	}
+	return 0, 0, io.EOF
 }
