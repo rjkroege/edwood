c6b669082be3444428fb253ba1919b1a3a3efb8e
Paul Lalonde <plalonde@nvidia.com>
2023-04-26 10:57:33 -0700

    fix dirname expansion

--- expandfile_win.go
+++ expandfile_win.go
@@ -15,8 +15,7 @@ var filenameRE = regexp.MustCompileAcme(`((([a-zA-Z]:|((\\\\|//)[a-zA-Z0-9.]*))?
 
 var quotedfilenameRE = regexp.MustCompileAcme(`('([a-zA-Z]:|((\\\\|//)[a-zA-Z0-9.]*))?((\\|/)?[^<>:*|?"\n]*)*')(:([0-9]+|(/[^ ']+)))?`)
 
-func findquotedcontext(t *Text, q0 int) (qq0, qq1 int) {
-	// Let's try a radical departure.  Start by getting a line.
+func getline(t *Text, q0 int) (qq0, qq1 int) {
 	qq0 = q0
 	qq1 = q0
 	for qq0 > 0 {
@@ -33,12 +32,18 @@ func findquotedcontext(t *Text, q0 int) (qq0, qq1 int) {
 		}
 		qq1++
 	}
+	return qq0, qq1
+}
+
+func findquotedcontext(t *Text, q0 int) (qq0, qq1 int) {
+	// Let's try a radical departure.  Start by getting a line.
+	qq0, qq1 = getline(t, q0)
 	if qq1 == qq0 { return q0, q0 }
 
 	n := qq1 - qq0
 	rb := make([]rune, n)
 	t.file.Read(qq0, rb[:n])
-	
+
 	found := quotedfilenameRE.FindForward(rb, 0, n, -1)
 	for _, pair := range found {
 		// qq0 is zero of the range returned.
@@ -51,116 +56,61 @@ func findquotedcontext(t *Text, q0 int) (qq0, qq1 int) {
 }
 
 func expandfile(t *Text, q0 int, q1 int, e *Expand) (success bool) {
-	amax := q1
-	if q1 == q0 {
-		// Check for being in a quoted string, find out if its a file.
-		qq0, qq1 := findquotedcontext(t, q0)
-		if qq0 != qq1 {
-			// Invariant: qq0 and qq1-1 are '
-			if expandfile(t, qq0+1, qq1-1, e) {
-				// We have a file.  If we have a colon following our qq1+1 quote
-				// we have to get it and add it to Expand.
-				cq1 := qq1
-				c := t.ReadC(cq1)
-				if c != ':' { // We don't have any address information here.  Just return e.
-					e.q0 = qq0
-					e.q1 = qq1
-					return true
-				}
-				cq1++
-				// collect the address
-				e.a0 = cq1
-				for cq1 < t.file.Nr() {
-					c := t.ReadC(cq1)
-					if !isaddrc(c) && !isregexc(c) && c != '\'' {
-						break
-					}
-					cq1++
-				}
-				e.a1 = cq1
-				q0 = qq0
-				q1 = cq1
-				e.q0 = q0
-				e.q1 = q1
-				return true
-			}
-		} else {
-			colon := int(-1)
-			// TODO(rjk): utf8 conversion work.
-			for q1 < t.file.Nr() {
-				c := t.ReadC(q1)
-				if !isfilec(c) {
-					break
-				}
-				if c == ':' {
-					colon = q1
-					break
-				}
-				q1++
-			}
-			for q0 > 0 {
-				c := t.ReadC(q0 - 1)
-				if !isfilec(c) && !isaddrc(c) && !isregexc(c) {
-					break
-				}
-				if colon < 0 && c == ':' {
-					colon = q0 - 1
-				}
-				q0--
-			}
-			// if it looks like it might begin file: , consume address chars after :
-			// otherwise terminate expansion at :
-			if colon >= 0 {
-				q1 = colon
-				if colon < t.file.Nr()-1 {
-					c := t.ReadC(colon + 1)
-					if isaddrc(c) {
-						q1 = colon + 1
-						for q1 < t.file.Nr() {
-							c := t.ReadC(q1)
-							if !isaddrc(c) {
-								break
-							}
-							q1++
-						}
-					}
-				}
-			}
-			if q1 > q0 {
-				if colon >= 0 { // stop at white space
-					for amax = colon + 1; amax < t.file.Nr(); amax++ {
-						c := t.ReadC(amax)
-						if c == ' ' || c == '\t' || c == '\n' {
-							break
-						}
-					}
-				} else {
-					amax = t.file.Nr()
-				}
+	n := 0
+	var found [][]int
+	var rb []rune
+	if q0 == q1 {
+		qq0, qq1 := getline(t, q0)
+		n = qq1 - qq0
+		if n == 0 {
+			return false
+		}
+		rb = make([]rune, n)
+		t.file.Read(qq0, rb[:n])
+		found = quotedfilenameRE.FindForward(rb, 0, n, 1)
+		if found == nil {
+			found = filenameRE.FindForward(rb, 0, n, 1)
+		}
+		for i, pair := range found {
+			// qq0 is zero of the range returned.
+			// The match is thus at qq0+pair[0:1], and (q0-qq0) must fall in that range
+			if q0-qq0 >= pair[0] && q0 - qq0 <= pair[1] {
+				q0, q1 = pair[0] + qq0, pair[1] + qq0
+				found = found[i:i+1]
+				break
 			}
 		}
-	}
-	amin := amax
-	e.q0 = q0
-	e.q1 = q1
-	n := q1 - q0
-	if n == 0 {
-		return false
-	}
-	// see if it's a file name
-	rb := make([]rune, n)
-	t.file.Read(q0, rb[:n])
+	} else {
+		n = q1 - q0
+		if n == 0 {
+			return false
+		}
+		rb = make([]rune, n)
+		t.file.Read(q0, rb[:n])
 
-	found := filenameRE.FindForward(rb, 0, n, 1)
-	if found == nil {
 		found = quotedfilenameRE.FindForward(rb, 0, n, 1)
+		if found == nil {
+			found = filenameRE.FindForward(rb, 0, n, 1)
+		}
+		if found != nil && found[0][0] != 0 {
+			return false
+		}
+	}
+	if found == nil {
+		return false
 	}
 	// Found now has a filename in group 2, address in group 9
 	nname := -1
+	e.q0 = q0
+	e.q1 = q1
+	amax := q1
+	amin := amax
 	filename := ""
 	if found != nil && found[0][4] != -1 {
 		nname = found[0][5]-found[0][4] 
 		filename = string(rb[found[0][4]:found[0][5]])
+		e.q0 = q0 + found[0][4]
+		e.q1 = q1 + found[0][5]
 	}
 	if found != nil && found[0][18] != -1 {
 		amin = found[0][18] + q0
