fd528e3baadce529f2b8cb9020271628a1292121
Paul Lalonde <plalonde@nvidia.com>
2023-05-03 09:03:28 -0700

    Fix rune count in X, x event messages

--- exec.go
+++ exec.go
@@ -13,6 +13,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"9fans.net/go/plan9"
 	"9fans.net/go/plan9/client"
@@ -223,9 +224,9 @@ func execute(t *Text, aq0 int, aq1 int, external bool, argt *Text) {
 			}
 		}
 		if a != "" {
-			t.w.Eventf("%c0 0 0 %d %v\n", c, len(a), a)
+			t.w.Eventf("%c0 0 0 %d %v\n", c, utf8.RuneCountInString(a), a)
 			if aa != "" {
-				t.w.Eventf("%c0 0 0 %d %v\n", c, len(aa), aa)
+				t.w.Eventf("%c0 0 0 %d %v\n", c, utf8.RuneCountInString(aa), aa)
 			} else {
 				t.w.Eventf("%c0 0 0 0 \n", c)
 			}
