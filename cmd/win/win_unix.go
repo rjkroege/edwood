09aa1ef37ec3929c5ad58afb5783f6786cb7e287
Paul Lalonde <1304784+paul-lalonde@users.noreply.github.com>
2023-05-04 03:07:39 -0700

    Golang win (#488)
    * new modules
    
    * A first cut at win.  Largely works?
    
    * fix typing buffer spill
    
    * Partial towards window name handling.  Blocked by tty handoff to child process
    
    * Bad formatting directive
    
    * Bad formatting directive
    
    * tty now points to the win!
    
    * Window label correctly generated; fix echo cancel bugs
    
    * fix dropcr bug
    
    * fix another dropcr bug
    
    * module updates for tests.
    
    * more modules
    
    * Shell selection
    
    * try to fix go.mod/go.sum
    
    * clean up uselessstransliterated breaks
    
    * More bad breaks
    
    * Fix a spelling error
    
    * And go fmt.  How do I run these static checks locally?
    
    * Factor isecho into platform-specifics
    
    * go fmt
    
    * Fix winid
    
    * go fmt
    
    * Implement sende - what a glaring omission this was
    
    ---------
    
    Co-authored-by: Paul Lalonde <plalonde@nvidia.com>

---
+++ cmd/win/win_unix.go
@@ -0,0 +1,21 @@
+// go:build (darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris)
+//go:build darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+
+package main
+
+import (
+	"os"
+
+	"github.com/pkg/term/termios"
+	"golang.org/x/sys/unix"
+)
+
+func isecho(fp *os.File) bool {
+	var ttmode unix.Termios
+	err := termios.Tcgetattr(fp.Fd(), &ttmode)
+	if err != nil {
+		debugf("tcgetattr: %v\n", err)
+	}
+	return ttmode.Lflag&unix.ECHO != 0
+}
